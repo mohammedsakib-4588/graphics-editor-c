37019bc19ba7628c2a6811847bb56e395aab92e9
mohammedsakib-4588 <mohammedsakib4588@gmail.com>
2026-06-10 14:14:29 +0000

    Added point drawing functionality

--- Graphics-miniproject.c
+++ Graphics-miniproject.c
@@ -24,10 +24,18 @@ void displayCanvas(int rows, int cols, char canvas[rows][cols])
         printf("\n");
     }
 }
-
+void drawPoint(int rows, int cols, char canvas[rows][cols], int r, int c)
+{
+    if(r >= 0 && r < rows && c >= 0 && c < cols)
+    {
+        canvas[r][c] = '*';
+    }
+}
 int main()
 {
     int rows, cols;
+    int choice;
+    int r, c;
 
     printf("Enter number of rows: ");
     scanf("%d", &rows);
@@ -38,7 +46,37 @@ int main()
     char canvas[rows][cols];
 
     initializeCanvas(rows, cols, canvas);
-    displayCanvas(rows, cols, canvas);
+
+    do
+    {
+        printf("\n===== 2D Graphics Editor =====\n");
+        printf("1. Draw Point\n");
+        printf("2. Display Canvas\n");
+        printf("3. Exit\n");
+        printf("Enter choice: ");
+        scanf("%d", &choice);
+
+        switch(choice)
+        {
+            case 1:
+                printf("Enter row and column: ");
+                scanf("%d %d", &r, &c);
+                drawPoint(rows, cols, canvas, r, c);
+                break;
+
+            case 2:
+                displayCanvas(rows, cols, canvas);
+                break;
+
+            case 3:
+                printf("Exiting...\n");
+                break;
+
+            default:
+                printf("Invalid choice!\n");
+        }
+
+    } while(choice != 3);
 
     return 0;
 }
